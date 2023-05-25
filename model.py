40ba5dd079a7114a7588940cef04b3a69f0705c3
楊鈞安 <yangjunan@yangjunandeMacBook-Pro.local>
2023-05-25 23:16:31 +0800

    first update

---
+++ model.py
@@ -0,0 +1,9 @@
+def curve():
+    y = x + c 
+def learning_curve():
+    
+def show_graph():
+    
+    plot
+def add_parameter():
+    
---
+++ parking_lot.py
@@ -0,0 +1,31 @@
+# limitation 
+
+# 平日40 假日60
+# 使用率和人流 的關係
+# 使用率 參數
+
+###### 討論
+# 市調 變動時間區間
+# 晚上 使用率高的停車塲 
+import model 
+import pandas as pd
+import 
+### 
+def past_data() : #(pd.dataframe, int, int, function) average_stop_time, now_price, model.curve
+    # call mariadb
+     
+def prediction() -> list: # return a list of 2 to 8 
+    
+def save_data():
+    # call mariadb
+def main( now_use_rate ):
+    if now_use_rate > pre:
+        new_parameter = (now_use_rate - pre) *  average_stop_time
+        model.add_parameter(new_parameter)
+        new_price = prediction()
+    else:
+        new_parameter = (pre - now_use_rate) *  average_stop_time
+        model.add_parameter(new_parameter)
+        new_price = prediction()
+if __init__ == "main":
+    
