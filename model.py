ce855c17bcdfd5e9ca458350ef559663880a4122
楊鈞安 <yangjunan@yangjunandeMacBook-Pro.local>
2023-05-28 14:45:34 +0800

    5-28-add showing graph and experiment

--- model.py
+++ model.py
@@ -1,9 +1,12 @@
-def curve():
-    y = x + c 
-def learning_curve():
-    
+def curve(use_rate):
+    return market_table[use_rate]
 def show_graph():
-    
-    plot
-def add_parameter():
-    
+    plt.scatter(df['Danceability'], df['Energy'], c=df['Cluster'], cmap='viridis')
+    plt.xlabel('Danceability')
+    plt.ylabel('Energy')
+    plt.title('K-means Clustering')
+    plt.show()
+def evaluation(lots_available, time, price_table):
+    # monthly renting  fixed_income
+    threshold = config.fixed_income + past_data(time) * lots_available * price_table(config.parking_lot)
+    return threshold 
